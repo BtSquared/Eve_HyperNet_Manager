3b1d99b0f891b6c7a7446388e115d5022c186829
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-01 15:53:55 -0500

    linked nav bar to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,20 @@ import { Route, Switch } from 'react-router-dom'
 //Pages
 import Home from './pages/Home'
 
+//components
+import NavBar from './components/NavBar'
+
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route />
-      </Switch>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/ships" />
+          <Route exact path="/history" />
+        </Switch>
+      </div>
     </div>
   )
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom'
 export default function NavBar() {
   return (
     <div>
-      <NavLink />
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/ships">Ships</NavLink>
+      <NavLink to="/history">Ship Performance History</NavLink>
     </div>
   )
 }
