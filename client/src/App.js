d722742c8a07f138b69da962bc9b194bdf282314
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-01 15:40:19 -0500

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route />
+      </Switch>
+    </div>
+  )
 }
 
 export default App
