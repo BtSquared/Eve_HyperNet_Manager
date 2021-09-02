ba36fd5175a8c8cd07cd086d8130fa42b30a69f5
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-01 22:57:21 -0500

    adjusted math values to make it more readable

--- controllers/controller.js
+++ controllers/controller.js
@@ -45,19 +45,23 @@ const MakeShips = async (req, res) => {
           listlength++
         }
       }
-      console.log(sum, ship.itemId)
       sum = sum / listlength
+      console.log(sum, ship.itemId)
 
       //math values
       const totalPrice = Math.floor(
         estValue[0].estimatedValue[index].EstPrice * 1.4
       )
+      const ticketBuy = Math.floor(totalPrice / 2)
       const payOut = Math.floor(totalPrice * 0.95)
+      const payOutHalf = payOut / 2
       const hyperCoreCount = Math.ceil(totalPrice / 5281172.73993)
-      const capitolRequired = hyperCoreCount * hyperCoreAvg + sum
-      const profit = Math.floor(payOut / 2 - hyperCoreCount * hyperCoreAvg)
-      const loss = Math.floor(capitolRequired - payOut / 2)
+      const hyperCoreCost = hyperCoreCount * hyperCoreAvg
+      const capitolRequired = hyperCoreCost + sum + ticketBuy
+      const profit = Math.floor(payOutHalf - hyperCoreCost)
+      const loss = Math.floor(hyperCoreCost + sum - payOutHalf)
       const shipOdds = profit / loss
+
       const newShip = await new Ship({
         shipName: ship.name,
         itemId: ship.itemId,
