cf1803e70529306434f6011b75c691b4e6acd59e
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-01 22:15:21 -0500

    added commas to shipcard numbers

--- client/src/components/ShipCard.js
+++ client/src/components/ShipCard.js
@@ -1,14 +1,21 @@
 export default function ShipCard(props) {
   const URL = `https://images.evetech.net/types/${props.itemId}/render?size=64`
+  const capitol = props.capitol.toLocaleString('en-US')
+  const profit = props.profit.toLocaleString('en-US')
+  const loss = props.loss.toLocaleString('en-US')
   return (
     <div className="shipCard">
       <img src={URL} />
-      <div>Name: {props.name}</div>
-      <div>Odds: {props.odds}</div>
-      <div>HyperCore Count: {props.core}</div>
-      <div>Capitol Required: {props.capitol}</div>
-      <div>Potential Profit: {props.profit}</div>
-      <div>Potential Loss: {props.loss}</div>
+      <div>
+        <div>Name: {props.name}</div>
+        <div>Odds: {props.odds}</div>
+        <div>HyperCore Count: {props.core}</div>
+      </div>
+      <div>
+        <div>Capitol Required: {capitol}</div>
+        <div>Potential Profit: {profit}</div>
+        <div>Potential Loss: {loss}</div>
+      </div>
     </div>
   )
 }
