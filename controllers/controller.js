77c6bce902d07f515740e1665bfb56976f9d5e29
Tanner Mitchell <TannerTMitchell@gmail.com>
2021-09-01 20:12:54 -0500

    added nukes for ships and estimated values

--- controllers/controller.js
+++ controllers/controller.js
@@ -25,7 +25,6 @@ const MakeShips = async (req, res) => {
     }
     hyperCoreAvg = hyperCoreAvg / 10
     const estValue = await EstVal.find({ name: 'EstimatedShipValue' })
-
     ShipIds.map(async (ship, index) => {
       const response = await axios.get(
         `https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&location_id=60003760&order_type=sell&page=1&type_id=${ship.itemId}`
@@ -50,44 +49,42 @@ const MakeShips = async (req, res) => {
       sum = sum / listlength
 
       //math values
-      setTimeout(async () => {
-        const totalPrice = Math.floor(
-          estValue[0].estimatedValue[index].EstPrice * 1.4
-        )
-        const ticketBuy = totalPrice / 2
-        const payOut = Math.floor(totalPrice * 0.95)
-        const hyperCoreCount = Math.ceil(totalPrice / 5281172.73993)
-        const capitolRequired = hyperCoreCount * hyperCoreAvg + sum
-        const profit = Math.floor(
-          payOut - ticketBuy * 0.05 - hyperCoreCount * hyperCoreAvg
-        )
-        const loss = Math.floor(
-          sum - ticketBuy * 0.05 - hyperCoreCount * hyperCoreAvg
-        )
-        const shipOdds = profit / loss
+      const totalPrice = Math.floor(
+        estValue[0].estimatedValue[index].EstPrice * 1.4
+      )
+      const ticketBuy = totalPrice / 2
+      const payOut = Math.floor(totalPrice * 0.95)
+      const hyperCoreCount = Math.ceil(totalPrice / 5281172.73993)
+      const capitolRequired = hyperCoreCount * hyperCoreAvg + sum
+      const profit = Math.floor(
+        payOut - ticketBuy * 0.05 - hyperCoreCount * hyperCoreAvg
+      )
+      const loss = Math.floor(
+        sum - ticketBuy * 0.05 - hyperCoreCount * hyperCoreAvg
+      )
+      const shipOdds = profit / loss
 
-        const newShip = await new Ship({
-          shipName: ship.name,
-          itemId: ship.itemId,
-          odds: shipOdds.toFixed(3),
-          coreCount: hyperCoreCount,
-          capitolReq: capitolRequired,
-          potentialProfit: profit,
-          potentialLoss: loss
-        })
-        await newShip.save()
-      }, 10000)
+      const newShip = await new Ship({
+        shipName: ship.name,
+        itemId: ship.itemId,
+        odds: shipOdds.toFixed(3),
+        coreCount: hyperCoreCount,
+        capitolReq: capitolRequired,
+        potentialProfit: profit,
+        potentialLoss: loss
+      })
+      await newShip.save()
     })
     setTimeout(async () => {
       const ships = await Ship.find()
       res.json({ ships })
-    }, 20000)
+    }, 10000)
   } catch (err) {
     console.log(err)
   }
 }
 
-const DeleteShips = async (req, res) => {
+const NukeShips = async (req, res) => {
   await Ship.deleteMany()
   const ships = await Ship.find()
   res.json({ ships })
@@ -137,11 +134,18 @@ const UpdateEstimatedValues = async (req, res) => {
   }
 }
 
+const NukeEstVal = async (req, res) => {
+  await EstVal.deleteMany()
+  const estVal = await EstVal.find()
+  res.json({ estVal })
+}
+
 module.exports = {
   GetShip,
   MakeShips,
-  DeleteShips,
+  NukeShips,
   GetEstimatedValue,
   PostEstimatedValue,
-  UpdateEstimatedValues
+  UpdateEstimatedValues,
+  NukeEstVal
 }
